f22a47f1f580fbcd3da784ef446c97e3e349430c
SWAGATAM04 <skundu012@gmail.com>
2023-07-26 16:00:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ resource "aws_instance" "web" {
   }
 
  
-  #provisioner "file" {
-  #  source      = "software.sh"
-  #  destination = "/tmp/software.sh"
-  #}
+  provisioner "file" {
+    source      = "software.sh"
+    destination = "/tmp/software.sh"
+  }
 
  #provisioner "remote-exec" {
   #  inline = [
