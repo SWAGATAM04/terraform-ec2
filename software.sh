99ae8ed839939284cfdef23736972072b7ad3b6a
SWAGATAM04 <skundu012@gmail.com>
2023-07-26 22:54:21 +0530

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo useradd --comment 'GitLab Runner' --create-home github-runner --shell /bin/bash
 sudo curl -fL https://install-cli.jfrog.io | sh
-sudo mkdir -p /home/gitlab-runner/actions-runner
-sudo curl -o /home/gitlab-runner/actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz
-sudo su - root -c "echo 'gitlab-runner   ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers"
+sudo mkdir -p /home/github-runner/actions-runner
+sudo curl -o /home/github-runner/actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz
+sudo su - root -c "echo 'github-runner   ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers"
 sudo yum install -y wget unzip git
 sudo curl -o /tmp/terraform_1.5.3_linux_amd64.zip -L https://releases.hashicorp.com/terraform/1.5.3/terraform_1.5.3_linux_amd64.zip
 sudo unzip /tmp/terraform_1.5.3_linux_amd64.zip -d /usr/local/bin/
@@ -15,16 +15,13 @@ echo 'export MAVEN_HOME=/opt/apache-maven-3.8.2' >> .bashrc
 echo 'PATH=$PATH:$MAVEN_HOME/bin' >> .bashrc
 echo 'export JAVA_HOME=/usr/lib/jvm/openlogic-openjdk-11-hotspot' >> .bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> .bashrc
-sudo cp .bashrc /home/gitlab-runner/.bashrc
-sudo curl -o /home/gitlab-runner/jboss-eap-7.4.0.zip -L  https://kubesphere-ohio-eks.s3.us-east-2.amazonaws.com/jboss-eap-7.4.0.zip
-sudo unzip /home/gitlab-runner/jboss-eap-7.4.0.zip -d /home/gitlab-runner/
-sudo chown -R gitlab-runner:gitlab-runner /home/gitlab-runner/
+sudo cp .bashrc /home/github-runner/.bashrc
+sudo curl -o /home/github-runner/jboss-eap-7.4.0.zip -L  https://kubesphere-ohio-eks.s3.us-east-2.amazonaws.com/jboss-eap-7.4.0.zip
+sudo unzip /home/github-runner/jboss-eap-7.4.0.zip -d /home/github-runner/
+sudo chown -R github-runner:github-runner /home/github-runner/
 sudo yum install docker libicu -y
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo chmod 777 /var/run/docker.sock
-sudo usermod -aG docker gitlab-runner
-sudo chown -R gitlab-runner:gitlab-runner /usr/local/bin/jf
-
-
-
+sudo usermod -aG docker github-runner
+sudo chown -R github-runner:github-runner /usr/local/bin/jf
